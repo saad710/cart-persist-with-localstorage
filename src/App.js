7ec883e23f5b1f2f9d7c1f8906c2e9e46df2dc7f
saad710 <ifrathasnat2@gmail.com>
2022-05-31 20:08:02 +0600

    cart-persist

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ProductList from "./component/ProductList";
+import Cart from "./component/Cart";
+
+function App() {
+  return (
+    <div className="App" >
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Cart() {
+  return (
+    <div>
+        <h3>Cart</h3>
+        {/* <p>Product Name : </p> */}
+    </div>
+  )
+}
+
+export default Cart
